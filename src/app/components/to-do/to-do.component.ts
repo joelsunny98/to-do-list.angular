d3f38f0e5661ca122583992b0644173fb924b554
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-06-26 12:03:09 +0530

    Created a basic dynamic Form

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -1,14 +1,41 @@
 import { CommonModule } from '@angular/common';
-import { Component, isStandalone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-to-do',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,
+    ReactiveFormsModule],
   templateUrl: './to-do.component.html',
   styleUrls: ['./to-do.component.scss']
 
 })
-export class ToDoComponent {
+export class ToDoComponent implements OnInit {
+  fields: string[] = [];
+  model = {
+    date: Date,
+    task: '',
+    remarks: ''
+  };
+  taskForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.buildForm();
+  }
+
+  buildForm() {
+    const formGroupFields = this.getFormControlField();
+    this.taskForm = new FormGroup(formGroupFields);
+    console.log(this.taskForm);
+  }
+
+  getFormControlField() {
+    const formGroupFields: { [key: string]: FormControl } = {};
+    for (var field of Object.keys(this.model)) {
+      formGroupFields[field] = new FormControl("");
+      this.fields.push(field);
+    }
+    return formGroupFields;
+  }
 }
