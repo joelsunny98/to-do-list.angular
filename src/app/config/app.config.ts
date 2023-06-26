3fd12c2d5069225703ab00a7a259b82256e43f64
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-06-26 15:55:37 +0530

    Removed Validators from Config

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -2,68 +2,68 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 export const HOLIDAYS_ARRAY = [
   {
-    date: new FormControl(new Date('2023-01-01'), Validators.required),
-    task: new FormControl('New Year', Validators.required),
+    date: new FormControl(new Date('2023-01-01')),
+    task: new FormControl('New Year'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-01-26'), Validators.required),
-    task: new FormControl('Republic Day', Validators.required),
+    date: new FormControl(new Date('2023-01-26')),
+    task: new FormControl('Republic Day'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-04-07'), Validators.required),
-    task: new FormControl('Good Friday', Validators.required),
+    date: new FormControl(new Date('2023-04-07')),
+    task: new FormControl('Good Friday'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-04-28'), Validators.required),
-    task: new FormControl('Ramzan', Validators.required),
+    date: new FormControl(new Date('2023-04-28')),
+    task: new FormControl('Ramzan'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-05-01'), Validators.required),
-    task: new FormControl('May Day', Validators.required),
+    date: new FormControl(new Date('2023-05-01')),
+    task: new FormControl('May Day'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-07-28'), Validators.required),
-    task: new FormControl('Bakrid', Validators.required),
+    date: new FormControl(new Date('2023-07-28')),
+    task: new FormControl('Bakrid'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-08-15'), Validators.required),
-    task: new FormControl('Independence Day', Validators.required),
+    date: new FormControl(new Date('2023-08-15')),
+    task: new FormControl('Independence Day'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-08-28'), Validators.required),
-    task: new FormControl('First Onam', Validators.required),
+    date: new FormControl(new Date('2023-08-28')),
+    task: new FormControl('First Onam'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-08-29'), Validators.required),
-    task: new FormControl('Thiruvonam', Validators.required),
+    date: new FormControl(new Date('2023-08-29')),
+    task: new FormControl('Thiruvonam'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-10-02'), Validators.required),
-    task: new FormControl('Gandhi Jayanti', Validators.required),
+    date: new FormControl(new Date('2023-10-02')),
+    task: new FormControl('Gandhi Jayanti'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-10-23'), Validators.required),
-    task: new FormControl('Mahanavami', Validators.required),
+    date: new FormControl(new Date('2023-10-23')),
+    task: new FormControl('Mahanavami'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-10-24'), Validators.required),
-    task: new FormControl('Vijayadasami', Validators.required),
+    date: new FormControl(new Date('2023-10-24')),
+    task: new FormControl('Vijayadasami'),
     remarks: new FormControl('')
   },
   {
-    date: new FormControl(new Date('2023-12-25'), Validators.required),
-    task: new FormControl('Christmas', Validators.required),
+    date: new FormControl(new Date('2023-12-25')),
+    task: new FormControl('Christmas'),
     remarks: new FormControl('')
   }
 ];
