08a3ef2d20584a3d98cff7d0f52e6c0616d52907
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-06-27 09:50:25 +0530

    Changed functionality to add task on click

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -34,32 +34,21 @@ export class ToDoComponent implements OnInit {
     this.buildForm();
   }
 
+  /**
+   * Method to build the Forms
+   */
   buildForm() {
-    const formGroupFields = this.getFormControlField();
-    this.taskForm = new FormGroup(formGroupFields);
-    this.taskArray.push(this.taskForm);
-    console.log(this.taskArray);
-  }
-
-  getFormControlField() {
-    const formGroupFields: { [key: string]: FormControl } = {};
-    for (var field of Object.keys(this.model)) {
-      formGroupFields[field] = new FormControl("");
-      this.fields.push(field);
-    }
-    return formGroupFields;
+    this.taskForm = this.formbuilder.group({
+      date: [Date, Validators.required],
+      task: ['', Validators.required],
+      remarks: ['']
+    });
   }
 
   /**
    * Method to add a new Task to FormArray
    */
   addFormGroup() {
-    const newForm = this.formbuilder.group({
-      date : [Date, Validators.required],
-      task: ['', Validators.required],
-      remarks: ['']
-    });
-
-    this.taskArray.push(newForm);
+    this.taskArray.push(this.taskForm);
   }
 }
