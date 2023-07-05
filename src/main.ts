af0c6a9b6cf5d5f3c80a991a576b5fe20dbbe5a6
austinsp135 <60867807+austinsp135@users.noreply.github.com>
2023-07-05 21:36:06 +0530

    Code to Bootstrap TodoComponent directly

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { ToDoComponent } from './app/components/to-do/to-do.component';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-import { AppModule } from './app/app.module';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(ToDoComponent)
+.catch((err)=>console.error(err));
